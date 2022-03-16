503cbfc45d2a3df73b9ed679b8615991fcb2a2a0
世元 李 <1498045907@qq.com>
2022-03-17 05:00:17 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using HslCommunication.MQTT;
+
+MqttServer Server = new MqttServer();
+
+Server.ClientVerification += (MqttSession mqttSession, string clientId, string userName, string passwrod) =>
+  {
+      if (userName == "Acite" && passwrod == "761834") return 0;
+
+      return 5;
+  };
+
+Server.ServerStart(54234);
