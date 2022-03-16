9ad8e348ba2cff4db8ff78a5d55d4d88bdd860d6
世元 李 <1498045907@qq.com>
2022-03-17 05:19:43 +0800

    Init

--- Program.cs
+++ Program.cs
@@ -4,8 +4,19 @@ MqttServer Server = new MqttServer();
 
 Server.ClientVerification += (MqttSession mqttSession, string clientId, string userName, string passwrod) =>
   {
-      if (userName == "Acite" && passwrod == "761834") return 0;
+      Console.Write(mqttSession.EndPoint.Address.ToString());
+      Console.Write("attempt to connect with username:");
+      Console.Write(userName);
+      Console.Write(" password: ");
+      Console.WriteLine(passwrod);
 
+      if (userName == "Acite" && passwrod == "761834") 
+      {
+          Console.WriteLine("ClientVerification Success");
+          return 0;
+      }
+
+      Console.WriteLine("ClientVerification Failed");
       return 5;
   };
 
