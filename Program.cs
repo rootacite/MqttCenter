630236a4d48ca31ce35510e71c02614c54175311
世元 李 <1498045907@qq.com>
2022-03-18 14:01:51 +0800

    Init

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using HslCommunication.MQTT;
+using System.Text;
 
 MqttServer Server = new MqttServer();
+Console.Write("ServiceStarted");
 
 Server.ClientVerification += (MqttSession mqttSession, string clientId, string userName, string passwrod) =>
   {
@@ -19,9 +21,22 @@ Server.ClientVerification += (MqttSession mqttSession, string clientId, string u
       Console.WriteLine("ClientVerification Failed");
       return 5;
   };
-
+Server.OnClientApplicationMessageReceive += (MqttSession session, MqttClientApplicationMessage message) =>
+{
+    Console.WriteLine(session.EndPoint.Address.ToString() + "says" + Encoding.UTF8.GetString(message.Payload));
+};
 Server.ServerStart(54234);
 while(true)
 {
-    _ = Console.ReadLine();
+    Thread.Sleep(500);
+    if(!Server.IsStarted)
+    {
+        try
+        {
+            Server.ServerClose();
+            Server.ServerStart();
+        }catch (Exception ex)
+        { }
+        Console.WriteLine("Serivice Restarted");
+    }
 }
